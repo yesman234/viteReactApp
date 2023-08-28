0f30395fee83d0a37b6a19dc9db380919d4ea9ff
yesman234 <only1marcellus@aol.com>
2023-08-28 12:10:04 -0700

    assetts added

---
+++ src/Index.jsx
@@ -0,0 +1,98 @@
+import { useState } from 'react'
+import ReactDOM from "react-dom/client";
+import './App.css'
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function Header(){
+   const style = {};
+  return <header className='header footer'><h1 style={style}>Fast React Pizza Co.</h1></header>
+}
+function Menu(){
+  return <main className='menu'>
+    <h2>Our Menu</h2>
+  </main>
+
+}
+function Footer(){
+  //& 
+const hour = new Date().getHours();
+const openHour =8;
+const closeHour =22;
+const isOpen =(hour >= openHour && hour <= closeHour);
+console.table(isOpen)
+return <footer className='footer'>
+  {new Date().toLocaleTimeString()} <span>We're open</span>
+</footer>
+}
+
+
+function Pizza(){
+  return (
+    <div>
+      <img src='pizzas/spinaci.jpg' alt="Pizzas spinaci"/>
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+
+  )
+}
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container">
+      <Header/>
+      <Menu/>
+      <Pizza />
+      <Footer/>
+       </div>
+    </>
+  )
+}
+
+export default App
